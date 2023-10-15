17d4ad2a2f992a6109d0fdf4c1ffaf0d7d4d318c
yingH95 <dragon5y@foxmail.com>
2023-10-15 14:51:21 -0700

    通过读取文档里的vid可以爬完系列弹幕

--- video_danmu.py
+++ video_danmu.py
@@ -36,19 +36,58 @@ async def get_dm_content(session, url):
         return new_list
 
 
-def write_dms_to_csv(vid, dms):
-    page_url = f'https://v.qq.com/x/cover/mzc00200ivo12rf/{vid}.html'
+def write_dms_to_csv(vid, dms, is_var):
+    page_url = f'https://v.qq.com/x/cover/mzc00200t7i1qwp/{vid}.html'
     page_html = requests.get(page_url).content
     bs = BeautifulSoup(page_html, 'html.parser')
     title = bs.title.contents[0].split("_")[0]
-    with open(f'dms/{title}_{datetime.now()}.csv', 'w') as csvf:
+    if is_var:
+        folder_name = title.split("《")[1].split("》")[0]
+    else:
+        folder_name = title
+    with open(f'dms/{folder_name}/{title}_{datetime.now()}.csv', 'w') as csvf:
         writer = csv.DictWriter(csvf, fieldnames=dms[0].keys())
         writer.writeheader()
         writer.writerows(dms)
 
 
+async def get_series_danmu():
+    list_name = input('file name: ')
+    is_variety = input('is variety?: ')
+    is_variety = is_variety.lower() == 'yes'
+    with open(f'series_list/{list_name}', 'r') as f:
+        video_list = f.read().splitlines()
+        video_list = list(filter(lambda a: not "#" == a[0], video_list))
+    is_content = input('just content?: ')
+    for vid in video_list:
+        print(f'--- Getting danmus for {vid} ---')
+        seg_index_url = f'https://dm.video.qq.com/barrage/base/{vid}'
+        r = requests.get(seg_index_url)
+        segment_index = r.json()['segment_index']
+        url_prefix = f'https://dm.video.qq.com/barrage/segment/{vid}/'
+        url_list = [url_prefix + seg['segment_name'] for index, seg in segment_index.items()]
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            if is_content == 'yes':
+                for url in url_list:
+                    tasks.append(asyncio.ensure_future(get_dm_content(session, url)))
+            else:
+                for url in url_list:
+                    tasks.append(asyncio.ensure_future(get_dm_seg(session, url)))
+
+            dms_seg_list = await asyncio.gather(*tasks)
+            all_dms = []
+            for dms_seg in dms_seg_list:
+                all_dms = all_dms + dms_seg
+            write_dms_to_csv(vid, all_dms, is_variety)
+            print(f'--- Finished collection for {vid} ---')
+    return 0
+
+
 async def main():
     vid = input('vid: ')
+    is_variety = input('is variety?: ')
+    is_variety = is_variety.lower() == 'yes'
     is_content = input('just content?: ')
     seg_index_url = f'https://dm.video.qq.com/barrage/base/{vid}'
     r = requests.get(seg_index_url)
@@ -69,10 +108,14 @@ async def main():
         all_dms = []
         for dms_seg in dms_seg_list:
             all_dms = all_dms + dms_seg
-        write_dms_to_csv(vid, all_dms)
+        write_dms_to_csv(vid, all_dms, is_variety)
         return all_dms
 
 if __name__ == '__main__':
     start_time = time.time()
-    asyncio.run(main())
+    run_list = input('run list? ').lower() == 'yes'
+    if run_list:
+        asyncio.run(get_series_danmu())
+    else:
+        asyncio.run(main())
     print("--- %s seconds ---" % (time.time() - start_time))
